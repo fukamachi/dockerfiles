6bfc1c7eaebeb76fab79e4fcdd426369721f262c
Eitaro Fukamachi <e.arrows@gmail.com>
2020-01-10 18:11:54 +0900

    Run 'docker push' for all tags at once.

--- publish.sh
+++ publish.sh
@@ -22,22 +22,17 @@ latest_version=$(basename $(cat $image/versions | sort -Vr | head -n 1))
 echo "Login to Docker Hub"
 echo $DOCKER_HUB_PASSWORD | docker login -u $owner --password-stdin
 
-echo "Publish $tagname"
-docker push $tagname
-
+echo "Create alias tags"
 if [ "$target" == "debian" ]; then
-  echo "Publish $owner/$image:$version"
   docker tag $tagname "$owner/$image:$version"
-  docker push $owner/$image:$version
 fi
 
 if [ "$latest_version" == "$version" ]; then
-  echo "Publish $owner/$image:latest-$target"
   docker tag $tagname "$owner/$image:latest-$target"
-  docker push $owner/$image:latest-$target
   if [ "$target" == "debian" ]; then
-    echo "Publish $owner/$image:latest"
     docker tag $tagname "$owner/$image:latest"
-    docker push $owner/$image:latest
   fi
 fi
+
+echo "Publish $owner/$image"
+docker push $owner/$image
