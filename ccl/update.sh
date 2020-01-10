51061eead8bc9b1726526df9f2ad25c09dd20770
Eitaro Fukamachi <e.arrows@gmail.com>
2020-01-10 23:27:02 +0900

    Delete old versions of ccl.

--- ccl/update.sh
+++ ccl/update.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 base="fukamachi/roswell"
 
-curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | cat versions - | sort -V | uniq > versions
+curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | head -n 3 | cat versions - | sort -V | uniq > versions
 
 latest_roswell=$(basename $(cat ../roswell/versions | sort -Vr | head -n 1))
 
