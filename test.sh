4b9f09b692ea14769d85a55340342bdb32d65fa4
Eitaro Fukamachi <e.arrows@gmail.com>
2020-01-19 11:37:52 +0900

    Use ENTRYPOINT.

--- test.sh
+++ test.sh
@@ -15,9 +15,8 @@ fi
 image=$1
 version=$2
 target=$3
-cmd=${4:-$1}
 
 tagname="$owner/$image:$version-$target"
 
 echo "Test $tagname"
-docker run --rm $tagname $cmd --version
+docker run --rm $tagname --version
