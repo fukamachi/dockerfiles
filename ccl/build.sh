811cf6f977facd3e50ddd14059ccffe6fbc48d95
Eitaro Fukamachi <e.arrows@gmail.com>
2021-02-03 18:58:51 +0900

    Build ccl only on linux/amd64.

--- ccl/build.sh
+++ ccl/build.sh
@@ -30,7 +30,7 @@ tagname="$owner/ccl:$version-$target"
 
 echo "Build $tagname"
 docker buildx build -t $tagname \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --platform linux/amd64 \
   --build-arg ROSWELL_IMAGE="$owner/roswell" \
   --build-arg ROSWELL_VERSION=$roswell_version \
   --build-arg PLATFORM=$target \
