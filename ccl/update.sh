519c95dc30e0ed1d2c92b17d79b73c59bac01d4d
Eitaro Fukamachi <e.arrows@gmail.com>
2021-10-29 04:07:50 +0000

    Fix the logic to check the new versions.

--- ccl/update.sh
+++ ccl/update.sh
@@ -4,7 +4,7 @@ set -ex
 
 cd `dirname $0`
 
-new_versions=( `curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | head -n 3 | grep -v "^$(cat versions | awk -F, '{ print $1 }')$" | sort -V` )
+new_versions=( `curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | head -n 3 | grep -v "$(cat versions | awk -F, '{ printf "\\|^%s$", $1 }' | cut -b 3-)" | sort -V` )
 
 latest_roswell=$(cat ../roswell/versions | sort -Vr | head -n 1 | awk -F, '{ print $1 }')
 
