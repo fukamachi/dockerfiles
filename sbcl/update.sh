307e3c3a80e9c37f4d144f98c0aacc7968bf0d12
Eitaro Fukamachi <e.arrows@gmail.com>
2021-12-02 02:28:10 +0000

    Refer the tags of github.com/sbcl/sbcl instead of roswell/sbcl_bin.

--- sbcl/update.sh
+++ sbcl/update.sh
@@ -4,7 +4,15 @@ set -ex
 
 cd `dirname $0`
 
-new_versions=( `curl -s https://api.github.com/repos/roswell/sbcl_bin/releases | jq -r '.[] | .tag_name' | sed -e 's/^v//' | grep -v "^$(cat versions | awk -F, '{ printf "\\\\|^%s$", $1 }' | cut -b 4-)" | sort -V` )
+if [ ! -d sbcl-git ]; then
+  git clone https://github.com/sbcl/sbcl sbcl-git
+fi
+
+cd sbcl-git
+
+new_versions=( `git tag --sort=taggerdate --no-merged=233b11d5623a08e7703a9818c3b86bb4e981a920 -l "sbcl-*" | grep -v sbcl-sbcl- | sed -e 's/sbcl-//' | grep -v "^$(cat ../versions | awk -F, '{ printf "\\\\|^%s$", $1 }' | cut -b 4-)" | sort -V` )
+
+cd ../
 
 latest_roswell=$(cat ../roswell/versions | sort -Vr | head -n 1 | awk -F, '{ print $1 }')
 
