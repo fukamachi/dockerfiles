ec1042ee0b2244a227a9385d321e0b7038c101e1
Eitaro Fukamachi <e.arrows@gmail.com>
2022-07-01 05:38:55 +0000

    Update the versions of base OSes.

--- roswell/update.sh
+++ roswell/update.sh
@@ -7,8 +7,8 @@ cd `dirname $0`
 new_versions=( `curl -s https://api.github.com/repos/roswell/roswell/releases\?per_page\=5 | jq -r '.[] | .tag_name' | sed -e 's/^v//' | grep -v "^$(cat versions | awk -F, '{ printf "\\\\|^%s$", $1 }' | cut -b 4-)" | sort -V` )
 
 debian_image="bullseye-slim"
-alpine_image="3.14"
-ubuntu_image="20.04"
+alpine_image="3.16"
+ubuntu_image="22.04"
 libcurl_version="libcurl4-gnutls-dev"
 
 for version in "${new_versions[@]}"; do
