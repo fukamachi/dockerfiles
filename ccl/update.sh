8d476fa4aa8cb3d4cef6a125d4321a685f349650
Eitaro Fukamachi <e.arrows@gmail.com>
2023-08-10 18:20:29 +0900

    Fix the filter for new versions of Clozure CL.

--- ccl/update.sh
+++ ccl/update.sh
@@ -5,7 +5,7 @@ set -ex
 cd `dirname $0`
 
 # Ignore 1.11.8
-new_versions=( `curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | head -n 3 | grep -v "^$(cat versions | awk -F, '{ printf "\\\\|^%s$", $1 }' | cut -b 4-)" | grep -v "^1.11.8$" | sort -V` )
+new_versions=( `curl -s "https://api.github.com/repos/roswell/ccl_bin/releases?per_page=10" | jq -r '.[] | .tag_name' | grep -E '^[0-9\\.]+$' | head -n 3 | grep -v "^\\($(cat versions | awk -F, '{ print $1 }' | sed -e 's/\./\\\\./g' | awk '{ printf "\\\\|%s", $1 }' | cut -b 3-)\\)$" | grep -v '^1\.11\.8$' | sort -V` )
 
 latest_roswell=$(cat ../roswell/versions | sort -Vr | head -n 1 | awk -F, '{ print $1 }')
 
